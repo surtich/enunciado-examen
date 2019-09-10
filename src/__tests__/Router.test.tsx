43bc333a1178d20f3436ef1ae96b024c42565113
Javier Pérez Arteaga <surtich@gmail.com>
2019-09-11 01:25:21 +0200

    [Navbar] Added links to Navbar component

--- src/__tests__/Router.test.tsx
+++ src/__tests__/Router.test.tsx
@@ -1,4 +1,4 @@
-import { waitForElement } from "@testing-library/react";
+import { wait, waitForElement } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { createMemoryHistory } from "history";
 import React from "react";
@@ -47,4 +47,22 @@ test("after click on logo navigate to home page", async () => {
   userEvent.click(logo);
   await waitForElement(() => getByTestId("home-page"));
   expect(queryByTestId("rooms-page")).not.toBeInTheDocument();
+});
+
+test("open vs close navbar menu", async () => {
+  const { debug, getByTestId, queryByTestId } = renderRouter(<App />);
+
+  const menu = getByTestId("menu");
+  const navbarLinks = getByTestId("navbar-links");
+  const roomsLink = getByTestId("rooms-link");
+
+  expect(navbarLinks).toHaveClass("nav__links"); // bad: implementation details
+  userEvent.click(menu); // open menu
+  await wait(() => expect(navbarLinks).toHaveClass("nav__links-show"));
+
+  userEvent.click(roomsLink);
+  await waitForElement(() => getByTestId("rooms-page"));
+
+  userEvent.click(menu); // close menu
+  await wait(() => expect(navbarLinks).toHaveClass("nav__links"));
 });
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
+import { FaAlignRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 export default class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
     return (
-      <nav className="navbar" data-testid="navbar">
-        <Link data-testid="logo" to="/">
-          <img src={logo} alt="Beach Resort" />
-        </Link>
+      <nav className="nav" data-testid="navbar">
+        <div className="nav__left">
+          <Link to="/" data-testid="logo">
+            <img src={logo} alt="Beach Resort" />
+          </Link>
+          <ul
+            className={this.state.isOpen ? "nav__links-show" : "nav__links"}
+            data-testid="navbar-links"
+          >
+            <li>
+              <Link to="/" data-testid="home-link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/rooms" data-testid="rooms-link">
+                Rooms
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="nav__right">
+          <button
+            type="button"
+            className="nav__btn"
+            onClick={this.handleToggle}
+            data-testid="menu"
+          >
+            <FaAlignRight className="nav__icon" />
+          </button>
+        </div>
       </nav>
     );
   }
